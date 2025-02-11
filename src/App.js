b62c5bdd487b17d7e856b5a4ab131c288baadd40
nishthasingh20 <singhnishtha206@gmail.com>
2025-02-11 13:00:35 +0530

    pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './Components/Header';
+import Homepage from './Pages/Homepage';
+import CoinPage from './Pages/CoinPage';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: '#14161a',
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='/coins/:id' element={<CoinPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import { AppBar, Toolbar } from '@material-ui/core'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <AppBar color='transparent' position='static'>
+      <Container>
+        <Toolbar>
+          
+        </Toolbar>
+      </Container>
+    </AppBar>
+  )
+}
+
+export default Header
