ba882df8c9b0372633cf8ecb4bd4c1a5ce986308
nishthasingh20 <singhnishtha206@gmail.com>
2025-02-13 11:38:15 +0530

    carousel13feb

--- src/Components/Banner/Carousel.js
+++ src/Components/Banner/Carousel.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core';
 import axios from 'axios';
 import { CryptoState } from '../../CryptoContext';
 import { TrendingCoins } from '../../config/api';
-import AliceCarousel from 'react-alice-carousel';
+import AliceCarousel, { Link } from 'react-alice-carousel';
 
 const useStyles = makeStyles((theme) => ({
     carousel: {
@@ -13,10 +13,14 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
+export function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 const Carousel = () => {
     const [trending, setTrending] = useState([]);
     const classes = useStyles();
-    const { currency } = CryptoState();
+    const { currency, symbol } = CryptoState();
 
     const fetchTrendingCoins = async () => {
         const { data } = await axios.get(TrendingCoins(currency));
@@ -30,7 +34,28 @@ const Carousel = () => {
     }, [currency]);
 
     const items = trending.map((coin) => {
-        
+        let profit = coin.price_change_percentage_24h >= 0;
+
+        return (
+            <Link className={classes.carouselItem} to={`/coins/${coin.id}`}>
+                <img
+                    src={coin?.image}
+                    alt={coin.name}
+                    height="80"
+                    style={{ marginBottom: 10 }}
+                />
+                <span>
+                    {coin?.symbol}
+                    &nbsp;
+                    <span>
+                        {profit && "+"} {coin?.price_change_percentage_24h?.toFixed(2) +"%"}
+                    </span>
+                </span>
+                <span style={{ fontSize: 22, fontWeight: 500}}>
+                    {symbol} {numberWithCommas(coin?.current_price.toFixed(2))}
+                </span>
+            </Link>
+        )
     })
     const responsive = {
         0: {
@@ -49,6 +74,7 @@ const Carousel = () => {
         autoPlayInterval={1000}
         animationDuration={1500}
         disableDotsControls
+        disableButtonsControls
         responsive={responsive}
         autoPlay
         items={items}
