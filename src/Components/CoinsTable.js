803cd7b872cdfa97c9909a1c245861e0b68371ce
nishthasingh20 <singhnishtha206@gmail.com>
2025-02-15 17:19:46 +0530

    tablestart

---
+++ src/Components/CoinsTable.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { CoinList } from '../config/api';
+import { CryptoState } from '../CryptoContext';
+import { Container, createTheme, ThemeProvider, Typography } from '@material-ui/core';
+
+const CoinsTable = () => {
+    const [coins, setCoins] = useState([]);
+    const[loading, setLoading] = useState(false);
+
+    const { currency } = CryptoState();
+
+    const fetchCoins = async () => {
+        setLoading(true);
+        const { data } = await axios.get(CoinList(currency));
+        setCoins(data);
+        setLoading(false);
+    };
+
+    console.log(coins);
+
+    useEffect(() => {
+        fetchCoins()
+    }, [currency]);
+
+    const darkTheme = createTheme({
+        palette: {
+            primary: {
+                main: "#fff",
+            },
+            type: "dark",
+        },
+    });
+
+  return <ThemeProvider theme={darkTheme}>
+    <Container style={{textAlign: "center"}}>
+        <Typography
+            variant='h4'
+            style={{margin: 18, fontFamily: "Bitter"}}>
+                CryptoCurrency Prices by Market Cap
+        </Typography>
+    </Container>
+  </ThemeProvider>
+};
+
+export default CoinsTable;
--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Banner from '../Components/Banner/Banner'
+import CoinsTable from '../Components/CoinsTable';
 
 const Homepage = () => {
   return <>
   <Banner />
+  <CoinsTable />
   </>
 };
 
