a299976b2a731f6424d328c62fc211737675f168
nishthasingh20 <singhnishtha206@gmail.com>
2025-02-19 12:27:16 +0530

    coinstart

--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -2,16 +2,21 @@ import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { CryptoState } from '../CryptoContext';
+import axios from 'axios';
+import { SingleCoin } from "../config/api";
 
 const CoinPage = () => {
   const { id } = useParams();
   const [ coin, setCoin ] = useState();
+  const formattedId = id?.toLowerCase();
 
   const { currency, symbol } = CryptoState();
   const fetchCoin = async () => {
-    const { data } = await axios.get(SingleCoin(id));
+    const { data } = await axios.get(SingleCoin(formattedId));
     setCoin(data);
   };
+
+  console.log(coin);
 
   useEffect(() => {
     fetchCoin()
@@ -20,6 +25,6 @@ const CoinPage = () => {
   return (
     <div>Coin Page</div>
   )
-}
+};
 
-export default CoinPage
+export default CoinPage;
