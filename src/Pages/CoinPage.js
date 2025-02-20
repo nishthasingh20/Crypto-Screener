6ce1bf0c7148da9f26b20ae2ed53feb2a0820ffe
nishthasingh20 <singhnishtha206@gmail.com>
2025-02-21 01:06:06 +0530

    coinInfo

--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -4,6 +4,18 @@ import { useParams } from 'react-router-dom';
 import { CryptoState } from '../CryptoContext';
 import axios from 'axios';
 import { SingleCoin } from "../config/api";
+import { makeStyles } from '@material-ui/core';
+import CoinInfo from '../Components/CoinInfo';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: "flex",
+    [theme.breakpoints.down("md")]: {
+      flexDirection: "column",
+      alignItems: "center",
+    },
+  },
+}));
 
 const CoinPage = () => {
   const { id } = useParams();
@@ -22,8 +34,17 @@ const CoinPage = () => {
     fetchCoin()
   }, [])
 
+  const classes = useStyles();
+
   return (
-    <div>Coin Page</div>
+    <div className={classes.container}>
+      <div className={classes.sidebar}>
+        {/* sidebar */}
+      </div>
+
+      {/* chart */}
+      <CoinInfo coin={coin} />
+    </div>
   )
 };
 
