492ee60e2e024f88776acc64702130245fba801d
nishthasingh20 <singhnishtha206@gmail.com>
2025-02-23 02:03:34 +0530

    info

--- src/Components/CoinInfo.js
+++ src/Components/CoinInfo.js
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { CryptoState } from '../CryptoContext';
+import axios from 'axios';
+import { HistoricalChart } from '../config/api';
+import { createTheme, makeStyles, ThemeProvider } from '@material-ui/core';
 
-const CoinInfo = () => {
-  return (
-    <div>CoinInfo</div>
-  )
+const useStyles = makeStyles((theme) => ({
+  container: {
+    width: "75%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 25,
+    padding: 40,
+    [theme.breakpoints.down("md")]: {
+      width: "100%",
+      marginTop: 0,
+      padding: 20,
+      paddingTop: 0,
+    },
+  },
+}));
+
+const CoinInfo = ({ coin }) => {
+  const [historicData, setHistoricData] = useState();
+  const [days, setDays] = useState(1);
+
+  const { currency } = CryptoState();
+
+  const fetchHistoricData = async () => {
+    const { data } = await axios.get(HistoricalChart(coin.id, days, currency))
+
+    setHistoricData(data.prices);
+  };
+
+  useEffect(() => {
+    fetchHistoricData();
+  }, [currency, days]);
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+
+  const classes = useStyles();
+
+  return <ThemeProvider theme={darkTheme}>
+    <div className = {classes.container}>
+      {/*chart*/}
+
+      {/*buttons*/}
+    </div>
+  </ThemeProvider>
 };
 
 export default CoinInfo;
