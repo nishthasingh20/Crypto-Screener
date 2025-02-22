315b7b352c2a42f14a7ff2df96913e2dd3e1cef6
nishthasingh20 <singhnishtha206@gmail.com>
2025-02-22 12:46:14 +0530

    singlecoin

--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -1,11 +1,10 @@
-import React, { useEffect } from 'react';
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { CryptoState } from '../CryptoContext';
 import axios from 'axios';
 import { SingleCoin } from "../config/api";
-import { makeStyles } from '@material-ui/core';
-import CoinInfo from '../Components/CoinInfo';
+import { Typography, LinearProgress, makeStyles } from '@material-ui/core';
+import { numberWithCommas } from '../Components/Banner/Carousel';
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -26,37 +25,122 @@ const useStyles = makeStyles((theme) => ({
     marginTop: 25,
     borderRight: "2px solid grey",
   },
+  heading: {
+    fontWeight: "bold",
+    marginBottom: 20,
+    fontFamily: "Bitter",
+  },
+  description: {
+    width: "100%",
+    fontFamily: "Bitter",
+    padding: 25,
+    paddingBottom: 15,
+    paddingTop: 0,
+    textAlign: "justify",
+  },
+  marketData: {
+    alignSelf: "start",
+    padding: 25,
+    paddingTop: 10,
+    width: "100%",
+    [theme.breakpoints.down("md")]: {
+      display: "flex",
+      justifyContent: "space-around",
+    },
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column",
+      alignItems: "center",
+    },
+    [theme.breakpoints.down("xs")]: {
+      alignItems: "start",
+    },
+  },
 }));
 
 const CoinPage = () => {
   const { id } = useParams();
-  const [ coin, setCoin ] = useState();
-  const formattedId = id?.toLowerCase();
-
+  const [coin, setCoin] = useState();
+  const [loading, setLoading] = useState(true);
+  const classes = useStyles();
   const { currency, symbol } = CryptoState();
+
   const fetchCoin = async () => {
-    const { data } = await axios.get(SingleCoin(formattedId));
-    setCoin(data);
+    try {
+      const { data } = await axios.get(SingleCoin(id));
+      setCoin(data);
+    } catch (error) {
+      console.error("Error fetching coin:", error);
+    } finally {
+      setLoading(false);
+    }
   };
 
-  console.log(coin);
-
   useEffect(() => {
-    fetchCoin()
-  }, [])
+    fetchCoin();
+  }, [id]);
 
-  const classes = useStyles();
+  if (loading) {
+    return <LinearProgress style={{ backgroundColor: "gold" }} />;
+  }
 
   return (
     <div className={classes.container}>
       <div className={classes.sidebar}>
-        {/* sidebar */}
+        <img
+          src={coin?.image.large}
+          alt={coin?.name}
+          height="200"
+          style={{ marginBottom: 20 }}
+        />
+        <Typography variant="h3" className={classes.heading}>
+          {coin?.name}
+        </Typography>
+        <Typography variant="subtitle1" className={classes.description}>
+          <div dangerouslySetInnerHTML={{ 
+            __html: coin?.description.en.split(". ")[0] 
+          }}/>
+        </Typography>
+        <div className={classes.marketData}>
+          <span style={{ display: "flex" }}>
+            <Typography variant="h5" className={classes.heading}>
+              Rank:
+            </Typography>
+            &nbsp; &nbsp;
+            <Typography variant="h5" style={{ fontFamily: "Bitter" }}>
+              {coin?.market_cap_rank}
+            </Typography>
+          </span>
+          <span style={{ display: "flex" }}>
+            <Typography variant="h5" className={classes.heading}>
+              Current Price:
+            </Typography>
+            &nbsp; &nbsp;
+            <Typography variant="h5" style={{ fontFamily: "Bitter" }}>
+              {symbol}{" "}
+              {numberWithCommas(
+                coin?.market_data.current_price[currency.toLowerCase()]
+              )}
+            </Typography>
+          </span>
+          <span style={{ display: "flex" }}>
+            <Typography variant="h5" className={classes.heading}>
+              Market Cap:{" "}
+            </Typography>
+            &nbsp; &nbsp;
+            <Typography variant="h5" style={{ fontFamily: "Bitter" }}>
+              {symbol}{" "}
+              {numberWithCommas(
+                coin?.market_data.market_cap[currency.toLowerCase()]
+                  .toString()
+                  .slice(0, -6)
+              )}
+              M
+            </Typography>
+          </span>
+        </div>
       </div>
-
-      {/* chart */}
-      <CoinInfo coin={coin} />
     </div>
-  )
+  );
 };
 
 export default CoinPage;
